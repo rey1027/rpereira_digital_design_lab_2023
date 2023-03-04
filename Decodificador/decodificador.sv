3164e8a7183dbad1a02729ae204386a1f4a7266f
rey1027 <62966013+rey1027@users.noreply.github.com>
2023-03-03 21:54:46 -0600

    Decodificador
    Código completo para la visualización en el display

--- Decodificador/decodificador.sv
+++ Decodificador/decodificador.sv
@@ -1,7 +1,7 @@
 module decodificador(A,B,C,D,S0,S1,S2,S3,S4);
 
- input A,B,C,D;
- output S0,S1,S2,S3,S4;
+ input logic A,B,C,D;
+ output logic S0,S1,S2,S3,S4;
  
  assign S0 =((A & B) | (A & C));
  assign S1 =(A & ~B & ~C);
