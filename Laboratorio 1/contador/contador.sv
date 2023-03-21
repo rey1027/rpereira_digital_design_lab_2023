28d4e8d05a48bfb3ec5c174824165e19be68980e
rey1027 <62966013+rey1027@users.noreply.github.com>
2023-03-20 21:43:08 -0600

    Laboratorio 1
    Introducción a los lenguajes de descripción
    de hardware

---
+++ Laboratorio 1/contador/contador.sv
@@ -0,0 +1,19 @@
+module contador#(parameter N = 6 ) 
+					(input  logic clk,reset,s,r,d,a,
+					 output logic [N-1 : 0] c);
+	 
+	 
+	 
+    always_ff @(negedge clk or posedge reset) begin
+		if (reset) c = 2**N - 1;
+		else if (a === 0) 
+		begin 
+			if (s === 0) c = c + 1;
+			else if (r === 0) c = c - 1;
+		end
+		else if (a)
+		begin 
+			if (d === 0) c = c - 1;
+		end	
+	 end  
+endmodule
